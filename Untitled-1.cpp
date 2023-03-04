bac635dd1d1655601e9952eacb1879b65ffdeb1b
86434 <54462380+86434@users.noreply.github.com>
2023-03-04 09:39:44 +0800

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+
+int main() {
+    string greeting = "hello, runoob";
+    cout << greeting;
+    cout << "\n";     // 换行符
+    string greeting2 = "hello, \
+                       runoob";
+    cout << greeting2;
+     for( ; ; )
+   {
+      printf("This loop will run forever.\n");
+   }
+   int max(int num1, int num2) 
